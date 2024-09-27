1e2247108f1c6dde9a6aab3626b6c49e6d425006
RomanKaptarUA <romankaptar82@gmail.com>
2024-09-27 16:17:57 +0300

    ю

--- script.js
+++ script.js
@@ -14,26 +14,26 @@
 
 
 
-const pool = document.getElementById('pool');
-const ball = document.getElementById('ball');
+// const pool = document.getElementById('pool');
+// const ball = document.getElementById('ball');
 
-pool.addEventListener('click', function(event) {
+// pool.addEventListener('click', function(event) {
   
-    const poolRect = pool.getBoundingClientRect();
-    const ballRect = ball.getBoundingClientRect();
-    const clickX = event.clientX - poolRect.left;
-    const clickY = event.clientY - poolRect.top;
-    const minX = ballRect.width / 2;
-    const minY = ballRect.height / 2;
-    const maxX = poolRect.width - ballRect.width / 2;
-    const maxY = poolRect.height - ballRect.height / 2;
+//     const poolRect = pool.getBoundingClientRect();
+//     const ballRect = ball.getBoundingClientRect();
+//     const clickX = event.clientX - poolRect.left;
+//     const clickY = event.clientY - poolRect.top;
+//     const minX = ballRect.width / 2;
+//     const minY = ballRect.height / 2;
+//     const maxX = poolRect.width - ballRect.width / 2;
+//     const maxY = poolRect.height - ballRect.height / 2;
 
-    const newX = Math.max(minX, Math.min(clickX, maxX));
-    const newY = Math.max(minY, Math.min(clickY, maxY));
+//     const newX = Math.max(minX, Math.min(clickX, maxX));
+//     const newY = Math.max(minY, Math.min(clickY, maxY));
 
-    ball.style.left = `${newX - ballRect.width / 2}px`;
-    ball.style.top = `${newY - ballRect.height / 2}px`;
-});
+//     ball.style.left = `${newX - ballRect.width / 2}px`;
+//     ball.style.top = `${newY - ballRect.height / 2}px`;
+// });
 
 
 
